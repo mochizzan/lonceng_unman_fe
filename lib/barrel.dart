aaf7afdc12bf5facf15ce4f6847a54749a6a71a3
mochizzan <izzanfirasyansyah2318@gmail.com>
2026-08-05 07:20:04 +0700

    feat: add barrel exports for notification feature

--- lib/barrel.dart
+++ lib/barrel.dart
@@ -4,5 +4,6 @@ export 'features/auth/barrel.dart';
 export 'features/data_initialization/barrel.dart';
 export 'features/home/barrel.dart';
 export 'features/jadwal/barrel.dart';
+export 'features/notification/barrel.dart';
 export 'features/profile/barrel.dart';
 export 'shared/barrel.dart';
---
+++ lib/features/notification/barrel.dart
@@ -0,0 +1,6 @@
+// Notification feature barrel exports
+export 'domain/entities/scheduled_notification_entity.dart';
+export 'domain/repositories/notification_repository.dart';
+export 'domain/services/notification_scheduler.dart';
+export 'presentation/cubit/notification_cubit.dart';
+export 'presentation/cubit/notification_state.dart';
